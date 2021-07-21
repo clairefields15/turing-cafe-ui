4ee6acb0d54953df07a643d8b26f9dfa852b7a40
Claire Fields <clairefields15@gmail.com>
2021-07-21 09:34:16 -0600

    Create modular file structure

---
+++ src/components/ReservationCard/ReservationCard.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import './ReservationCard.css';
+
+export default ReservationCard;
---
+++ src/components/Reservations/Reservations.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import './Reservations.css';
+
+export default Reservations;
